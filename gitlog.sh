9143a51a229057cc144d866e658b19788259812c
Nicolas Bossard <nicolas.bossard@orange.com>
2024-09-08 10:35:41 +0200

    bug gitlog

--- gitlog.sh
+++ gitlog.sh
@@ -1,2 +1,2 @@
 cd ~/storage/documents/obsperso
-log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset%n' --abbrev-commit --date=relative --branches
+git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset%n' --abbrev-commit --date=relative --branches
