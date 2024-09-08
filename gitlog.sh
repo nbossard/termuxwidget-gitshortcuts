f3e1a534cb10b2995d95280decc9b0d49546eab1
Nicolas Bossard <nicolas.bossard@orange.com>
2024-09-08 10:42:59 +0200

    adding git log compact format

--- gitlog.sh
+++ gitlog.sh
@@ -1,2 +1,9 @@
 cd ~/storage/documents/obsperso
-git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset%n' --abbrev-commit --date=relative --branches
+
+#compact display for phones
+
+git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s' --abbrev-commit --date=relative --branches
+
+# Alternative smarter display for tablets, with date and committer name and white lines between commits
+#
+#git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset%n' --abbrev-commit --date=relative --branches
